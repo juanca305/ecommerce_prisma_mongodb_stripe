7a98889876a9f878dd8bbb604fa7cb5301019c5b
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2024-01-18 09:51:17 -0500

    First Setup for Reviews

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -2,6 +2,7 @@ import Container from '@/app/components/Container';
 import React from 'react'
 import ProductDetails from './ProductDetails';
 import { product } from '@/utils/product';
+import ListRating from '../ListRating';
 
 interface IPrams {
     productId?:string
@@ -13,6 +14,10 @@ const Product = ({params} : {params: IPrams}) => {
     <div className='p-8'>
         <Container>
             <ProductDetails product = {product}/>
+            <div className=' flex flex-col mt-20 gap-4'>
+              <div>Add Ratings</div>
+              <ListRating product={product}/>
+            </div>
         </Container>
     </div>
   )
