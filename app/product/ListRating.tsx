d25cbf5a1750d501eb88c52fc47982c9f0d68612
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2024-01-18 10:37:24 -0500

    Add Avatar feature to Reviews Section

--- app/product/ListRating.tsx
+++ app/product/ListRating.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Heading from '../components/Heading';
 import moment from 'moment';
 import { Rating } from '@mui/material';
+import Avatar from '../components/Avatar';
 
 interface ListRatingProps {
   product: any
@@ -17,7 +18,7 @@ const ListRating: React.FC<ListRatingProps> = ({ product }) => {
         {product.reviews && product.reviews.map((review: any) => {
           return <div key={review.id} className=' max-w-[300px]'>
               <div className=' flex items-center gap-2'>
-                <div>Avatar</div>
+                <Avatar src={review.user.image}/>
                 <div className=' font-semibold'>{review?.user.name}</div>
                 <div className=' font-light'>{moment(review.createdDate).fromNow()}</div>
               </div>
