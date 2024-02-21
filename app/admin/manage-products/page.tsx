86fb771d67ad519f9dcf1cb5cfbe4ef8c9f1392d
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2024-02-21 05:55:12 -0500

    Manage products OK

--- app/admin/manage-products/page.tsx
+++ app/admin/manage-products/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import ManageProductsClient from './ManageProductsClient'
+import getProducts from '@/actions/getProducts'
+import { getCurrentUser } from '@/actions/getCurrentUser'
+import NullData from '@/app/components/NullData'
 
-const ManageProducts = () => {
+const ManageProducts = async() => {
+
+  const products = await getProducts({category: null});
+  const currentUser = await getCurrentUser();
+
+  if(!currentUser || currentUser.role !== 'ADMIN') {
+    return <NullData title='Oops! Access denied'/>
+  }
+
   return (
-    <div>ManageProducts</div>
+    <div><ManageProductsClient products = {products}/></div>
   )
 }
 
