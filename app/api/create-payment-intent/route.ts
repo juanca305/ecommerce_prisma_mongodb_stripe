c0b70f21f7f8b82754a370e65e624c21fb25c8df
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2024-03-20 09:42:27 -0400

    Added 'use server at the top of this file

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,5 +1,6 @@
 
-export const dynamic = "force-static";
+'use server'
+//export const dynamic = "force-static";
 import Stripe from "stripe";
 import prisma from "@/lib/prismadb";
 import { NextResponse } from "next/server";
