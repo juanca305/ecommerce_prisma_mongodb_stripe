a2cfaefde20d86dc73e020e73d7b14e8bfff5341
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2024-03-20 10:01:38 -0400

    use server deleted

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,14 +1,11 @@
 
-'use server'
+//'use server'
 //export const dynamic = "force-static";
 import Stripe from "stripe";
 import prisma from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 import { CartProductType } from "@/app/product/[productId]/ProductDetails";
 import {getCurrentUser} from "@/actions/getCurrentUser";
-
-
-
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, 
 {
