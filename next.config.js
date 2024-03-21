f02253b2d5505e60756a90ea1746fdbcd4d27a0e
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2024-03-21 09:07:44 -0400

    Recover at OK

--- next.config.js
+++ next.config.js
@@ -1,16 +1,25 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: ['firebasestorage']
+//     },
+// };
+
+
+// module.exports = {
+//     images: {
+//       remotePatterns: ['firebasestorage.googleapis.com'],
+//     },
+//   }
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['firebasestorage']
+      domains: ["firebasestorage.googleapis.com", "lh3.googleusercontent.com"],
     },
-};
-
-
-module.exports = {
-    images: {
-      remotePatterns: ['firebasestorage.googleapis.com'],
-    },
-  }
+  };
+  
+  module.exports = nextConfig;
 
 
 
