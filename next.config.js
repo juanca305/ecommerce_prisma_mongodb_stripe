cac94e7b36642fdb17fadfe006d44ef9f0130d2f
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2024-03-21 06:49:42 -0400

    Added staticPageGenerationTimeout: 1000 at next.config.js

--- next.config.js
+++ next.config.js
@@ -27,3 +27,10 @@ module.exports = nextConfig
 //module.exports = { eslint: { ignoreDuringBuilds: true } }
 
 
+module.exports = {
+    // time in seconds of no pages generating during static
+    // generation before timing out
+    staticPageGenerationTimeout: 1000,
+  }
+
+
